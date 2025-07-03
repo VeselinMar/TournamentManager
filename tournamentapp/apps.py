5a1ffc2a6e5dd69c6aea7262b1868127710f07ab
VeselinMar <veselin.martinov@gmail.com>
2025-07-03 14:27:31 +0200

    give verbose name to app

--- tournamentapp/apps.py
+++ tournamentapp/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class TournamentappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tournamentapp'
+    verbose_name = "Tournament Manager"
     
     def ready(self):
         # Import signals to ensure they are registered
