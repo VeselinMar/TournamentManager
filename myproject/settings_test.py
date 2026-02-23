3da218b268367e9b8f1196e3c67ed098cefe7346
VeselinMar <veselin.martinov@gmail.com>
2026-02-23 11:43:13 +0100

    adjust workflow and settings for testing

--- myproject/settings_test.py
+++ myproject/settings_test.py
@@ -3,6 +3,8 @@ from .settings import *
 AZURE_CONTAINER = "test-container"
 AZURE_ACCOUNT_NAME = "test"
 AZURE_ACCOUNT_KEY = "test"
+SECRET_KEY = "test-secret-key"
+
 
 PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.MD5PasswordHasher",
