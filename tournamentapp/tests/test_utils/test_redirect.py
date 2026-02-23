3187cea6378e1cd594e088e274e96dff1b9fa317
Veselin <veselin.martinov@gmail.com>
2026-02-23 11:37:30 +0100

    Dev (#23)
    * fix workflow
    
    * fix workflow
    
    * add test coverage, add testing for account utils address accessing db during app initialization

---
+++ tournamentapp/tests/test_utils/test_redirect.py
@@ -0,0 +1,37 @@
+import pytest
+from django.urls import reverse
+from tournamentapp.models import Tournament
+from accounts.utils import get_post_login_redirect
+
+@pytest.mark.django_db
+def test_redirects_to_existing_active_tournament(django_user_model):
+    # Create user
+    user = django_user_model.objects.create_user(
+        email="testuser@abv.bg",
+        password="pass"
+    )
+
+    # Create unfinished tournament
+    tournament = Tournament.objects.create(
+        name="Summer Cup",
+        owner=user,
+        is_finished=False
+    )
+
+    url = get_post_login_redirect(user)
+
+    expected_url = reverse("tournament-detail", kwargs={"pk": tournament.pk})
+    assert url == expected_url
+
+
+@pytest.mark.django_db
+def test_redirects_to_create_when_no_active_tournament(django_user_model):
+    user = django_user_model.objects.create_user(
+        email="testuser2@abv.bg",
+        password="pass"
+    )
+
+    url = get_post_login_redirect(user)
+
+    expected_url = reverse("tournament-create")
+    assert url == expected_url
