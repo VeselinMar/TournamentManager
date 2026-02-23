7d8184dd2ef5d8604d99f039dcc51cc2dd96d301
VeselinMar <veselin.martinov@gmail.com>
2026-02-23 11:30:16 +0100

    add test coverage, add testing for account utils address accessing db during app initialization

---
+++ myproject/settings_test.py
@@ -0,0 +1,16 @@
+from .settings import *
+
+AZURE_CONTAINER = "test-container"
+AZURE_ACCOUNT_NAME = "test"
+AZURE_ACCOUNT_KEY = "test"
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.MD5PasswordHasher",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
