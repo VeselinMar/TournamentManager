f86872499e2e54a7877f277898c2f460b61a2141
VeselinMar <veselin.martinov@gmail.com>
2026-03-28 16:22:44 +0100

    config react public view; adapt urls, views, create spa.html

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
+  plugins: [react()],
+  base: '/static/spa/',
+
+  build: {
+    outDir: path.resolve(__dirname, '../tournamentapp/static/spa'),
+    emptyOutDir: true,
+    manifest: true,
+  },
   server: {
+    port: 5173,
     proxy: {
       '/media': {
         target: 'http://localhost:8000',
